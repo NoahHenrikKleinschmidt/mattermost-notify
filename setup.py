fdd606f5b42c5e228ee0e78a22d42906712548b7
Noah Kleinschmidt <Noah.kleinschmidt@unibe.ch>
2025-01-23 12:27:05 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mattermost-notify',
+    package_dir={'': '.'},
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here
+        "requests"
+    ],
+    entry_points={
+        'console_scripts': [
+            'mattermost-notify=mattermost_notify.cli:main',
+        ],
+    },
+    author='Noah Kleinschmidt',
+    author_email='noah.kleinschmidt@unibe.ch',
+    description='A tool to send notifications to Mattermost',
+    url='https://github.com/yourusername/mattermost-notify',
+)
