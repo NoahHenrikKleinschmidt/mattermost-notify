91e001d8cfc16a3873f42038cf61c5a5622c1240
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2025-01-29 09:21:34 +0100

    udpate version

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='notify-mattermost',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    package_dir={'': '.'},
-    version='0.1.2',
+    name="notify-mattermost",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    package_dir={"": "."},
+    version="0.1.3",
     packages=find_packages(),
     install_requires=[
         # Add your dependencies here
         "requests",
-        "argparse"
+        "argparse",
     ],
     entry_points={
-        'console_scripts': [
-            'mattermost-notify=mattermost_notify.cli:main',
+        "console_scripts": [
+            "mattermost-notify=mattermost_notify.cli:main",
         ],
     },
-    author='Noah Kleinschmidt',
-    author_email='noah.kleinschmidt@unibe.ch',
-    description='A tool to send notifications to Mattermost',
-    url='https://github.com/NoahHenrikKleinschmidt/mattermost-notify',
+    author="Noah Kleinschmidt",
+    author_email="noah.kleinschmidt@unibe.ch",
+    description="A tool to send notifications to Mattermost",
+    url="https://github.com/NoahHenrikKleinschmidt/mattermost-notify",
 )
